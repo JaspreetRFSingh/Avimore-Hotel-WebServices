c4af109fc1c5f8dbecbd63f1d878d949dfdfc939
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-04-20 22:25:29 +0530

    ...

---
+++ Northstar.WS/Services/DefaultRoomService.cs
@@ -0,0 +1,23 @@
+﻿using Northstar.WS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northstar.WS.Services
+{
+    public class DefaultRoomService : IRoomService
+    {
+        //private readonly _context;
+
+        public DefaultRoomService()
+        {
+
+        }
+
+        public Task<Room> GetRoomAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Northstar.WS/Services/IRoomService.cs
@@ -0,0 +1,13 @@
+﻿using Northstar.WS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northstar.WS.Services
+{
+    public interface IRoomService
+    {
+        Task<Room> GetRoomAsync(Guid id);
+    }
+}
