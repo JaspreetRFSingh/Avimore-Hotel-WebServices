83d59c1c32dfaa1854834db177bcbe740c48284f
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-04-23 23:21:55 +0530

    Added basic SQL scripting

---
+++ SQL Construct/InsertQuery.sql
@@ -0,0 +1,19 @@
+insert into Room ([Name], [Rate]) values ('Budget', 1200)
+
+INSERT INTO [dbo].[FacilityAddress]
+           ([Street]
+           ,[City]
+           ,[Country])
+     VALUES
+           ('23 Model Town', 'Jalandhar', 'Punjab')
+
+INSERT INTO [dbo].[Hotel]
+           ([Title]
+           ,[Tagline]
+           ,[Email]
+           ,[LocationId])
+     VALUES
+           ('Tuli hospitality Services', 'Service  First', 'tulih@gmail.com', 3)
+
+Insert into Hotel_Room (HotelId, RoomId) values (4,4)
+
---
+++ SQL Construct/SelectQuery.sql
@@ -0,0 +1,4 @@
+select * from FacilityAddress
+Select * from Hotel
+select * from Room
+select * from Hotel_Room
