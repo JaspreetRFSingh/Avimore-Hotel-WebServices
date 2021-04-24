f9f484450e7a852a5a72557619130d4bb013e720
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-04-25 01:28:37 +0530

    Added unit test support

---
+++ Northstar.WS.Test/RoomsControllerTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Northstar.WS.Controllers;
+
+namespace Northstar.WS.Test
+{
+    [TestClass]
+    public class RoomsControllerTest
+    {
+        RoomsController roomsControllerMock = new RoomsController();
+
+        [TestMethod]
+        public void GetRooms_GetAllRooms()
+        {
+        }
+    }
+}
