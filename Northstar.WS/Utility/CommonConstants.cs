b79a8c4271e2c309d6a12d768b7a99176fb88221
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-05-01 19:45:11 +0530

    added CommonConstants.cs

---
+++ Northstar.WS/Utility/CommonConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northstar.WS.Utility
+{
+    public static class CommonConstants
+    {
+        #region Resource name constants for Controllers
+        public static string Room = "Room";
+        public static string Hotel = "Hotel";
+        #endregion
+
+        public static string ResourcePlaceHolder = "<resource>";
+        public static string ResourceIdPlaceHolder = "<resourceId>";
+        public static Dictionary<int, string> CustomErrorResponses = new Dictionary<int, string>()
+        {
+            {301, "<resource> not found with <resourceId>"},
+            {302, "Error!" }
+        };
+    }
+}
