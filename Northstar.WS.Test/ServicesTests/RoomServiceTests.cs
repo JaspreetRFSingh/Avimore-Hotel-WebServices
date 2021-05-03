da87125b296ae80925af48eb9cc31c6fe104d3b4
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-05-03 13:51:44 +0530

    added async method test

--- Northstar.WS.Test/ServicesTests/RoomServiceTests.cs
+++ Northstar.WS.Test/ServicesTests/RoomServiceTests.cs
@@ -3,8 +3,11 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Northstar.WS.Models;
 using Northstar.WS.Services;
+using Northstar.WS.Test.Providers;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Northstar.WS.Test.ServicesTests
 {
@@ -13,7 +16,7 @@ namespace Northstar.WS.Test.ServicesTests
     {
 
         [TestMethod]
-        public void GetRooms_GetAllRooms()
+        public void GetRooms_fetches_all_rooms()
         {
             var data = new List<Room>
             {
@@ -57,5 +60,41 @@ namespace Northstar.WS.Test.ServicesTests
             mockSet.Verify(m => m.Add(It.IsAny<Room>()), Times.Once());
             mockContext.Verify(m => m.SaveChanges(), Times.Once());
         }
+
+        [TestMethod]
+        public async Task GetRoomByIdAsync_fetches_a_room_by_id()
+        {
+            //ARRANGE
+            var data = new List<Room>
+            {
+                new Room { RoomId = 101, Name = "Deluxe",Rate = 2300 },
+                new Room { RoomId = 102, Name = "ExeZ", Rate = 2500 },
+                new Room { RoomId = 103, Name = "Basic", Rate = 2000 }
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Room>>(); mockSet.As<IDbAsyncEnumerable<Room>>()
+                 .Setup(m => m.GetAsyncEnumerator())
+                 .Returns(new TestDbAsyncEnumerator<Room>(data.GetEnumerator()));
+
+            mockSet.As<IQueryable<Room>>()
+                .Setup(m => m.Provider)
+                .Returns(new TestDbAsyncQueryProvider<Room>(data.Provider));
+            mockSet.As<IQueryable<Room>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Room>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Room>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<AvimoreDBContext>();
+            mockContext.Setup(c => c.Rooms).Returns(mockSet.Object);
+
+            //ACT
+            var service = new RoomService(mockContext.Object);
+            var room = await service.GetRoomByIdAsync(101);
+            
+
+            //ASSERT
+            Assert.AreEqual("Deluxe", room.Name);
+            Assert.AreEqual(2300, room.Rate);
+        }
+
     }
 }
