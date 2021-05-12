0004cb15195037bd7de3d8dc9e838580a13db3f9
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-05-12 07:38:31 +0530

    Added SearchTerm.cs

---
+++ Northstar.WS/Infrastructure/SearchTerm.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Northstar.WS.Infrastructure
+{
+    /// <summary>
+    /// Class representing the search query
+    /// </summary>
+    public class SearchTerm
+    {
+        public string Name { get; set; }
+        public string Operator { get; set; }
+        public string Value { get; set; }
+        public bool IsValidSyntax { get; set; }
+    }
+}
