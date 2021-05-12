9db422b160e88f83af0b3f7ce30f85a5d3e5e5d9
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-05-12 08:31:07 +0530

    Created SearchOptions and a delegate class to Process the search terms

---
+++ Northstar.WS/Infrastructure/SearchOptionsProcessor{T}.cs
@@ -0,0 +1,104 @@
+﻿
+using Northstar.WS.Infrastructure.CustomAttributes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Northstar.WS.Infrastructure
+{
+    public class SearchOptionsProcessor<T>
+    {
+        private string[] _searchQuery;
+        public SearchOptionsProcessor(string[] searchQuery)
+        {
+            _searchQuery = searchQuery;
+        }
+
+        public IEnumerable<SearchTerm> GetAllTerms()
+        {
+            if (_searchQuery == null) yield break;
+
+            foreach (var expression in _searchQuery)
+            {
+                if (string.IsNullOrEmpty(expression)) continue;
+                var tokens = expression.Split(' ');
+
+                if (tokens.Length < 3)
+                {
+                    yield return new SearchTerm
+                    {
+                        IsValidSyntax = false,
+                        Name = expression
+                    };
+                    continue;
+                }
+
+                yield return new SearchTerm
+                {
+                    IsValidSyntax = true,
+                    Name = tokens[0],
+                    Operator = tokens[1],
+                    Value = string.Join(" ", tokens.Skip(2))
+                };
+            }
+        }
+
+        public IEnumerable<SearchTerm> GetValidTerms()
+        {
+            var searchTerms = GetAllTerms()
+                .Where(x=>x.IsValidSyntax)
+                .ToArray();
+
+            if (!searchTerms.Any()) yield break;
+
+            var declaredTerms = GetTermsFromModel();
+
+            foreach (var term in searchTerms)
+            {
+                var declaredTerm = declaredTerms.SingleOrDefault(x => x.Name.Equals(term.Name, System.StringComparison.OrdinalIgnoreCase));
+                if (declaredTerm == null) continue;
+                yield return new SearchTerm
+                {
+                    IsValidSyntax = term.IsValidSyntax,
+                    Name = declaredTerm.Name,
+                    Operator = term.Operator,
+                    Value = term.Value
+                };
+            }
+
+        }
+
+        public IQueryable<T> Apply(IQueryable<T> query)
+        {
+            var terms = GetValidTerms().ToArray();
+            if (!terms.Any()) return query;
+            var modifiedQuery = query;
+            foreach (var term in terms)
+            {
+                var propertyInfo = ExpressionHelper.GetPropertyInfo<T>(term.Name);
+                var obj = ExpressionHelper.Parameter<T>();
+
+                var left = ExpressionHelper.GetPropertyExpression(obj, propertyInfo);
+                var right = Expression.Constant(term.Value);
+
+                var comparisonExpression = Expression.Equal(left, right);
+
+                var lambdaExpression = ExpressionHelper.GetLambda<T, bool>(obj, comparisonExpression);
+
+                modifiedQuery = ExpressionHelper.CallWhere(modifiedQuery, lambdaExpression);
+
+            }
+
+            return modifiedQuery;
+        }
+
+        private static IEnumerable<SearchTerm> GetTermsFromModel()
+        {
+            return typeof(T).GetTypeInfo().DeclaredProperties
+                .Where(p => p.GetCustomAttributes<SearchableAttribute>().Any())
+                .Select(p => new SearchTerm { Name = p.Name });
+        }
+
+    }
+}
---
+++ Northstar.WS/Models/SearchOptions{T}.cs
@@ -0,0 +1,37 @@
+﻿using Northstar.WS.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Northstar.WS.Models
+{
+    public class SearchOptions<T> : IValidatableObject
+    {
+        public string[] Search { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var processor = new SearchOptionsProcessor<T>(Search);
+
+            var validTerms = processor.GetValidTerms().Select(p => p.Name);
+            var invalidTerms = processor.GetAllTerms().Select(p => p.Name)
+                .Except(validTerms, StringComparer.OrdinalIgnoreCase);
+
+            foreach (var term in invalidTerms)
+            {
+                yield return new ValidationResult(
+                    $"Invalid search term: '{term}'",
+                    new[] { nameof(Search) }
+                    );
+            }
+        }
+
+        public IQueryable<T> Apply(IQueryable<T>query)
+        {
+            var processor = new SearchOptionsProcessor<T>(Search);
+            return processor.Apply(query);
+        }
+
+    }
+}
